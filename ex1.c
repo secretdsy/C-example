be9f74d03d47d0759c73b8e593d0ce55bde95a13
Doyoung Kim <secretdsy@naver.com>
2016-10-08 02:49:09 +0900

    ch10

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int N = 0;
+
+    printf("Change Decimal to Hexa Decimal\n");
+    printf("Decimal = ");
+    scanf("%d", &N);
+    printf("Hexa Decimal = %x\n", N);
+    return 0;
+}
